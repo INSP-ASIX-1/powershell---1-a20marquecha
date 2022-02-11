961efa2e9c7db7cadbdd5babd8e841f78c30d0bb
a20marquecha <95306579+a20marquecha@users.noreply.github.com>
2022-02-11 19:16:20 +0100

    Correcio 9 V2

--- 09.ps1
+++ 09.ps1
@@ -1,6 +1,6 @@
 #si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO
 
-if ($val[0] -eq $val[1]){
+if ($args[0] -eq $args[1]){
   Write-Output "OK"
   }
 else{
